14ff229ff794ab64f2014a25d4905d74f3fc4298
Alexies838 <72218655@idexpasco.edu.pe>
2025-04-14 15:07:17 -0500

    Add files via upload

---
+++ button.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btnslud");
+
+const parra = document.querySelector(".rpt");
+
+btn.addEventListener("click", function() {
+    parra.textContent = "Hola es un gusto tenerte por aqui";
+});
